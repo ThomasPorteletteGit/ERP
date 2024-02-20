947755c7f7a3f4653d959b5964c968adebe0e389
Thomas Portelette <thomas.portelette@etu.unice.fr>
2024-02-20 15:29:59 +0100

    header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import logo from '../assets/img/logo.png';
-import { Link } from 'react-router-dom';
+import logo from '../assets/img/logoBleu.png';
+import homeButton from '../assets/img/accueil.png'
+import backArrow from '../assets/img/en-arriere.png'
+import scaleButton from '../assets/img/agrandir.png'
+import profilePic from '../assets/img/profilePic.png'
 
 const Header = () => {
     return (
         <>
-
             <div className='header'>
-                <img id="logo" src={logo} alt="logo" />
-
-                <Link to='/dashboard'>
-                    <button id="backBtn">⬅️ Retour</button>
-                </Link>
-
+                <img id="logo" className='dashboardImages' src={logo} alt="logo" /> 
+                <img id="homeButton" className='dashboardImages' src={homeButton} alt="bouton accueil" />
+                <img id="backArrow" className='dashboardImages' src={backArrow} alt="fleche retour" />
+                <img id="scaleButton" className='dashboardImages' src={scaleButton} alt="bouton agrandir" />
+                <div className='dashboardImages' id="profilePicBackground">
+                    <img id="profilePic" src={scaleButton} alt="photo de profil" />
+                </div>
             </div>
         </>
     );
