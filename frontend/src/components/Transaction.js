92d94bff44daacf8da6540a91bfc2302c58e5bd8
Rayan Outili <rayan.outili@etu.unice.fr>
2024-02-29 22:20:00 +0100

    style transaction fidèle maquette

--- frontend/src/components/Transaction.js
+++ frontend/src/components/Transaction.js
@@ -1,43 +1,53 @@
 import React from 'react';
+import horloge from '../assets/img/lhorloge.png';
 
 const Transaction = () => {
 
     // à récup dans la bd
     const transactions = [
-        { id: 1, heure: '12:30', total: 50, type: 'CB', statut: 'Validé' },
-        { id: 2, heure: '15:45', total: 20, type: 'Espèce', statut: 'En attente' },
-        { id: 2, heure: '15:45', total: 20, type: 'Espèce', statut: 'En attente' },
-        { id: 2, heure: '15:45', total: 20, type: 'Espèce', statut: 'En attente' },
-        { id: 2, heure: '15:45', total: 20, type: 'Espèce', statut: 'En attente' },
-        { id: 2, heure: '15:45', total: 20, type: 'Espèce', statut: 'En attente' },
-        { id: 2, heure: '15:45', total: 20, type: 'Espèce', statut: 'En attente' },
-        { id: 2, heure: '15:45', total: 20, type: 'Espèce', statut: 'En attente' },
-        { id: 2, heure: '15:45', total: 20, type: 'Espèce', statut: 'En attente' },
-        { id: 2, heure: '15:45', total: 20, type: 'Espèce', statut: 'En attente' },
-        { id: 2, heure: '15:45', total: 20, type: 'Espèce', statut: 'En attente' },
-        { id: 2, heure: '15:45', total: 20, type: 'Espèce', statut: 'En attente' },
-        { id: 2, heure: '15:45', total: 20, type: 'Espèce', statut: 'En attente' },
-        { id: 2, heure: '15:45', total: 20, type: 'Espèce', statut: 'En attente' },
+        { id: 1, heure: '12:30', total: '50,50', type: 'CB', statut: 'Validé', isLoss: false },
+        { id: 2, heure: '15:45', total: '20,05€', type: 'ESP', statut: 'En attente', isLoss: true },
+        { id: 1, heure: '12:30', total: '50,50', type: 'CB', statut: 'Validé', isLoss: false },
+        { id: 2, heure: '15:45', total: '20,05€', type: 'ESP', statut: 'En attente', isLoss: true },
+        { id: 1, heure: '12:30', total: '50,50', type: 'CB', statut: 'Validé', isLoss: false },
+        { id: 2, heure: '15:45', total: '20,05€', type: 'ESP', statut: 'En attente', isLoss: true },
+        { id: 2, heure: '15:45', total: '20,05€', type: 'ESP', statut: 'En attente', isLoss: true },
+        { id: 1, heure: '12:30', total: '50,50', type: 'CB', statut: 'Validé', isLoss: false },
+        { id: 2, heure: '15:45', total: '20,05€', type: 'ESP', statut: 'En attente', isLoss: true },
+        { id: 1, heure: '12:30', total: '50,50', type: 'CB', statut: 'Validé', isLoss: false },
+        { id: 2, heure: '15:45', total: '20,05€', type: 'ESP', statut: 'En attente', isLoss: true },
+        { id: 1, heure: '12:30', total: '50,50', type: 'CB', statut: 'Validé', isLoss: false },
+        { id: 2, heure: '15:45', total: '20,05€', type: 'ESP', statut: 'En attente', isLoss: true },
+        { id: 1, heure: '12:30', total: '50,50', type: 'CB', statut: 'Validé', isLoss: false },
     ];
+
 
     // permet de récup la couleur en fonction du statut
     const getStatusColor = (statut) => {
         return statut === 'Validé' ? 'green' : 'red';
     };
 
+    const getPaymentTypeClass = (type) => {
+        return type === 'CB' ? 'cb-payment' : type === 'ESP' ? 'esp-payment' : '';
+    };
+
     return (
         <section id="transaction">
-            <h2 className='component-title'>Transaction</h2>
+            <div className="top_container">
+                <h2 className='component-title'>💸 Transaction</h2>
+            </div>
             <div className='transaction-container'>
                 <div className='last-transaction'>
                     <p>Dernière transaction</p>
 
-                    <input type="date" id="datePicker" name="datePicker" />
+                    <div className='input-select-container'>
+                        <input type="date" id="datePicker" name="datePicker" />
 
-                    <select id="typeTransaction" name="typeTransaction">
-                        <option value="cb">CB</option>
-                        <option value="espece">Espèce</option>
-                    </select>
+                        <select id="typeTransaction" name="typeTransaction">
+                            <option value="cb">CB</option>
+                            <option value="espece">ESP</option>
+                        </select>
+                    </div>
 
                 </div>
                 <div className='transaction-details'>
@@ -54,11 +64,27 @@ const Transaction = () => {
                         <tbody>
                             {transactions.map(transaction => (
                                 <tr key={transaction.id}>
-                                    <td>{transaction.id}</td>
-                                    <td>{transaction.heure}</td>
-                                    <td>{transaction.total}</td>
-                                    <td>{transaction.type}</td>
-                                    <span className={`statut-transac ${getStatusColor(transaction.statut)}`}></span>                            </tr>
+                                    <td>
+                                        <span className={`fleche-container ${transaction.isLoss ? 'loss' : 'no-loss'}`}>
+                                            <span className="fleche" />
+                                            {transaction.id}
+                                        </span>
+                                    </td>
+
+
+                                    <td id="horloge-hour">
+                                        <span className='hour-transac'>
+                                            <img id="lhorloge" src={horloge} alt="clock" />
+                                            {transaction.heure}
+                                        </span>
+                                    </td>
+                                    <td className='prix-transac'>{transaction.total}</td>
+                                    <td>
+                                        <span className={`payment-type ${getPaymentTypeClass(transaction.type)}`}>
+                                            {transaction.type}
+                                        </span>
+                                    </td>
+                                    <td><span className={`statut-transac ${getStatusColor(transaction.statut)}`}></span>       </td>                     </tr>
                             ))}
                         </tbody>
                     </table>
