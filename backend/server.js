49164d5f08b59db3861737dbbae577e41d9d10ee
Thomas Portelette <thomas.portelette@etu.unice.fr>
2024-03-06 17:38:44 +0100

    update pour le dao, il est plus comprehensible

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const DAO = require('./src/data/DAO');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/api', (req, res) => {
-    res.json({ message: 'Jason Derulo' });
-});
 
+const DAO = require('./src/data/DAO');
+
+const dao = new DAO();
 
 const PORT = process.env.PORT || 5000;
+
+app.get('/api', (req, res) => {
+    res.json({ dao: dao.dbIp });
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
