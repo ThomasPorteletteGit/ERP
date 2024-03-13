116adbd205eedb1969ccbd6d0e70df75a81dbd99
Thomas Portelette <thomas.portelette@etu.unice.fr>
2024-03-13 14:55:04 +0100

    Update de l'architecture du code avec passage des fonctions a l'interieur des routes dans des fichiers a part

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+const incidentRoutes = require('./src/database/incidents/incident');
+const stockEnergieRoutes = require('./src/database/stocks/stockEnergie');
+const stockProduitsRoutes = require('./src/database/stocks/stockProduits');
+const transactionRoutes = require('./src/database/transactions/transaction');
+
+app.use('/incident', incidentRoutes);
+app.use('/stockEnergie', stockEnergieRoutes);
+app.use('/stockProduits', stockProduitsRoutes);
+app.use('/transaction', transactionRoutes);
+
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
-
-
-const DAO = require('./src/data/DAO');
-
-const dao = new DAO();
-
 const PORT = process.env.PORT || 5000;
-
-app.get('/api', (req, res) => {
-    res.json({ dao: dao.dbIp });
-});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
---
+++ backend/src/database/stocks/stockFunctions.js
@@ -0,0 +1,76 @@
+const DAO = require('../../data/DAO');
+const dao = new DAO();
+
+// Fonctions pour les energies
+function getEnergieIds(req, res) {
+    dao.select('id', 'ProduitEnergie', "type='Energie'", (result) => {
+        res.send(result.rows);
+    });
+}
+
+function getEnergieId(req, res) {
+    let name = req.body.name;
+    dao.select('id', 'ProduitEnergie', `nom='${name}'`, (result) => {
+        res.send(result.rows);
+    });
+}
+
+function getEnergieStock(req, res) {
+    dao.select('*', 'ProduitEnergie', "type='Energie'", (result) => {
+        res.send(result.rows);
+    });
+}
+
+function reapprovisionnerEnergie(req, res) {
+    let id = req.body.id;
+    let quantite = req.body.quantite;
+    dao.update('ProduitEnergie', `quantite = quantite + ${quantite}`, `id = ${id}`, (result) => {
+        res.send(result);
+    });
+}
+
+// Fonctions pour les produits
+
+function getProduitIds(req, res) {
+    dao.select('id', 'ProduitEnergie', "type='Produit'", (result) => {
+        res.send(result.rows);
+    });
+}
+
+function getProduitId(req, res) {
+    let name = req.body.name;
+    dao.select('id', 'ProduitEnergie', `nom='${name}'`, (result) => {
+        res.send(result.rows);
+    });
+}
+
+function getProduitStock(req, res) {
+    dao.select('*', 'ProduitEnergie', "type='Produit'", (result) => {
+        res.send(result.rows);
+    });
+}
+
+function reapprovisionnerProduit(req, res) {
+    let id = req.body.id;
+    let quantite = req.body.quantite;
+    dao.update('ProduitEnergie', `quantite = quantite + ${quantite}`, `id = ${id}`, (result) => {
+        res.send(result);
+    });
+}
+
+module.exports = {
+    energies:
+    {
+        getEnergieIds,
+        getEnergieId,
+        getEnergieStock,
+        reapprovisionnerEnergie
+    },
+    produits:
+    {
+        getProduitIds,
+        getProduitId,
+        getProduitStock,
+        reapprovisionnerProduit
+    }
+};
