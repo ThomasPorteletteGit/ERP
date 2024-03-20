7f3abbf394d314bbb44dd3ba57fd0c151dab1c5f
Essalah Sabra <sabra.essalah@etu.unice.fr>
2024-03-20 15:56:05 +0100

    Début du service Parking : affichage des places de parking (en cours)

--- frontend/src/components/ServicesGrand.js
+++ frontend/src/components/ServicesGrand.js
@@ -1,6 +1,9 @@
 import React from "react";
 import services from "./Services";
 
+import ReactDOMServer from 'react-dom/server'
+import ServiceParking from "./ServiceParking";
+
 const ServicesGrand = () => {
     return (
         
@@ -11,32 +14,51 @@ const ServicesGrand = () => {
 
             <hr/>
             <div className="button_container_grand_services">
-
-                    <button className="serviceParkingButton">
-                        <h3> Parking</h3>
-                        
-                    </button>
-
-                    <button className="serviceCoVoiturageButton">
-                        <h3> Co-voiturage</h3>
-                       
-                    </button>
-
-                    <button className="serviceLivraisonButton">
-                        <h3> Livraison</h3>
-                         
-                    </button>
-  
-                    <button className="serviceAutreButton">
-                        <h3> Autres </h3>
-                       
-                    </button>
-                </div>
+                    <button className="bouttonsServiceGrand">Parking</button>
+                    <button className="bouttonsServiceGrand">Co-voiturage</button>
+                    <button className="bouttonsServiceGrand">Livraison</button>
+                    <button className="bouttonsServiceGrand">Autres</button>
+            </div>
           
-                </div>
-         
-         
-    );
+        </div>         
+    );   
+    
 };
+
+document.addEventListener("click", function (event) {
+    if (event.target.classList.contains("bouttonsServiceGrand")) {
+        const buttonText = event.target.textContent;
+        switch (buttonText) {
+            case "Parking":
+                console.log("Bouton Service -> parking cliqué");
+                // TODO: Ajouter code pour afficher les places de parking
+                afficherPlacesParking();
+                break;
+            case "Co-voiturage":
+                console.log("Bouton 'Co-voiturage' cliqué");
+                break;
+            case "Livraison":
+                console.log("Bouton 'Livraison' cliqué");
+                break;
+            case "Autres":
+                console.log("Bouton 'Autres' cliqué");
+                break;
+            default:
+                console.log("Bouton inconnu cliqué" + buttonText);
+                break;
+        }
+        event.preventDefault();
+    }
+
+    function afficherPlacesParking() {
+        const divGeneral = document.getElementsByClassName("dashboard-right")[0];
+        let saveDiv = divGeneral.innerHTML;
+        console.log("Afficher les places de parking");
+        
+        divGeneral.innerHTML = ReactDOMServer.renderToString(<ServiceParking />);
+        divGeneral.style.display = "block";
+
+    }
+});
 
 export default ServicesGrand;
