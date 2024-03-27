a8f71edaf01f57e1666715dc57c26d70156296c2
TORRI Clara <clara.torri@etu.unice.fr>
2024-03-27 12:10:54 +0100

    continuation carte

---
+++ frontend/src/components/CarteMembre.js
@@ -0,0 +1,15 @@
+const CarteMembre = () => {
+    return (
+        <section id="">
+            <div className="composantGrandCarte">
+                
+                <h2 className="component_title">Carte Membre</h2>
+                <hr></hr>
+                
+            </div>
+        </section>
+            
+    );
+
+}
+export default CarteMembre;
