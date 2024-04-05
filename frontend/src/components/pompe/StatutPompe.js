efd1abb9116d72ebebd71ff87cae06775103e69c
Griffonnet Matthieu <matthieu.griffonnet@etu.unice.fr>
2024-04-05 10:57:14 +0200

    Slider + image carte

--- frontend/src/components/pompe/StatutPompe.js
+++ frontend/src/components/pompe/StatutPompe.js
@@ -8,21 +8,27 @@ const StatutPompe = ({ numeroPompe, carburant, quantite, prix }) => {
     return (
         <div className={`statut-pompe ${isAvailable ? 'disponible' : 'indisponible'}`}>
             <div className="info-pompe">
-                <img className="pompe-image" src={pump} alt={`${numeroPompe}`} />
+                <img className="pompe-image" src={pump} alt={`${numeroPompe}`}/>
                 <p className="numero">{numeroPompe}</p>
                 <p className="carburant">{carburant}</p>
             </div>
             <div className='dispo-qte-prix'>
                 <div className="disponibilite">
-                    <div className={`rond ${isAvailable ? 'vert' : 'rouge'}`} />
+                    <div className={`rond ${isAvailable ? 'vert' : 'rouge'}`}/>
                 </div>
                 <div className="quantite-prix">
                     <p className="quantite">{quantite} L</p>
                     <p className="prix">{prix} €</p>
                 </div>
+                <div className="slider_Button">
+                    <label className="switch">
+                        <input className="chk" type="checkbox"/>
+                        <span className="slider"></span>
+                    </label>
+                </div>
+                </div>
             </div>
-        </div >
-    );
-};
+            );
+            };
 
-export default StatutPompe;
+            export default StatutPompe;
