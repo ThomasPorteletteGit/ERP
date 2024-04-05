307a3af3394df01212eb51be1319b560fb0772be
Griffonnet Matthieu <matthieu.griffonnet@etu.unice.fr>
2024-04-05 11:57:13 +0200

    Modification visuel carte

--- frontend/src/components/paiement/ChoixPaiement.js
+++ frontend/src/components/paiement/ChoixPaiement.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import energies from '../../assets/img/bgcarteenergie.png';
+import cash from '../../assets/img/cash.png';
+import creditCard from '../../assets/img/cb.png';
 
 const ChoixPaiement = () => {
     return (
@@ -6,9 +9,18 @@ const ChoixPaiement = () => {
             <div className='choixPaiement'>
                 <h2 className='view-title'>Choix du paiement</h2>
                 <div className='paymentChoice'>
-                    <button id="esp" className='cdpbouton'>Espèces</button>
-                    <button id="cb" className='cdpbouton'>Carte bancaire</button>
-                    <button id="carteEnergie" className='cdpbouton'>Carte énergie</button>
+                    <button id="esp" className='cdpbouton'>
+                        <p className="carte_text">CASH</p>
+                        <img src={cash} id="emoji_cash" alt="cash"/>
+                    </button>
+                    <button id="cb" className='cdpbouton'>
+                        <p className="carte_text">CB</p>
+                        <img src={creditCard} id="emoji_cb" alt="Cb"/>
+                    </button>
+                    <button id="carteEnergie" className='cdpbouton'>
+                        <p className="carte_text">ENERGIE</p>
+                        <img src={energies} id="emoji_energie" alt="Carte Energie" />
+                    </button>
                     {/* <button id="autre" className='cdpbouton'>Autres</button> */}
                 </div>
             </div >
