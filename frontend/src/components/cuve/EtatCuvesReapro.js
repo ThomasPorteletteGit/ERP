ace018c4ff2d1ba891ac113a3e0dd721bb612eb7
Tom DA COSTA <tom.da-costa@etu.unice.fr>
2024-04-08 15:48:13 +0200

    etatsdescuves css

--- frontend/src/components/cuve/EtatCuvesReapro.js
+++ frontend/src/components/cuve/EtatCuvesReapro.js
@@ -1,29 +1,34 @@
 import React from "react";
 
 const EtatCuvesReapro = () => {
-    return(
+    return (
         <div id="cuvesPrix">
             <h3>Demande de réaprovisionnement</h3>
-            <div className="Divflex">
-                <div id="divPrix">
-                    <div className="Divflex">
-                        <input type="checkbox" id="ReaproEsp-97" name="ReaproEsp-97"/>
+            <div className="Etatcuvediv">
+                <div id="divPrixEtat">
+                    <div className="defautreaprodiv">
+                        <input type="checkbox" id="ReaproEsp-97" name="ReaproEsp-97" />
                         <label for="ReaproEsp-97"> ESP-97</label>
                     </div>
-                    <div className="Divflex">
-                        <input type="checkbox" id="ReaproGazole" name="ReaproGazole"/>
+                    <div className="defautreaprodiv">
+                        <input type="checkbox" id="ReaproGazole" name="ReaproGazole" />
                         <label for="ReaproGazole"> GAZOLE</label>
                     </div>
-                    <div className="Divflex">
-                        <input type="checkbox" id="ReaproDiesel" name="ReaproDiesel"/>
+                    <div className="defautreaprodiv">
+                        <input type="checkbox" id="ReaproDiesel" name="ReaproDiesel" />
                         <label for="ReaproDiesel"> DIESEL</label>
                     </div>
-                    <div className="Divflex">
-                        <input type="checkbox" id="ReaproGPL" name="ReaproGPL"/>
+                    <div className="defautreaprodiv">
+                        <input type="checkbox" id="ReaproGPL" name="ReaproGPL" />
                         <label for="ReaproGPL"> GPL</label>
                     </div>
+                    <div className="defautreaprodiv">
+                        <input type="checkbox" id="ReaproE85" name="ReaproE85" />
+                        <label for="ReaproE85"> E85</label>
+                    </div>
+
                 </div>
-                <div className="DivBlock">
+                <div className="btnEtat">
                     <button id="btnValiderReapro">Valider</button>
                     <button id="btnAnnulerReapro">Annuler</button>
                 </div>
