810e8e9b597cb362976407303517c6b155d27116
TORRI Clara <clara.torri@etu.unice.fr>
2024-04-08 15:05:58 +0200

    carte membre/energie smallIcons ok

--- frontend/src/components/carteEnergie/AjouterCarte.js
+++ frontend/src/components/carteEnergie/AjouterCarte.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReactDOMServer from 'react-dom/server'
-import ListeClient from "./ListeClient";
-import AvantageCarte from "./AvantageCarte";
+import CarteEnergie from "./CarteEnergie";
+import SmallIcons from "../SmallIcons";
 
 //a faire logique bouton enregistrer
 const AjouterCarte = () => {
@@ -14,13 +14,10 @@ const AjouterCarte = () => {
             <div className="composantGrand">
                 <div className="composantGrandCarteEM">
                     <div className="Top_Component_Grand">
+                        <button id="buttonReturn" className="buttonCarte">Retour</button>
                         <h2 className="component_title">Ajouter une carte</h2>
                         <hr></hr>
                     </div>
-
-                    <button id="buttonCarteEM" className="buttonCarte">Voir liste des clients</button>
-                    <button id="buttonCarteEM" className="buttonCarte">Ajouter une carte</button>
-
                     <form>
                         <div className="infoClientCarte">
                             <h3>Nom :</h3>
@@ -51,22 +48,16 @@ document.addEventListener("click", function (event) {
     if (event.target.classList.contains("buttonCarte")) {
         const buttonText = event.target.textContent;
         switch (buttonText) {
-            case "Voir liste client":
-                console.log("Bouton liste client -> liste client cliqué");
-                afficherClients();
-                break;
-            case "Avantage carte énergie":
-                console.log("Bouton avantage cliqué");
-                voirAvantage();
-                break;
             case "Enregistrer":
                 console.log("Bouton enregister cliqué");
-
                 break;
             case "Annuler":
                 console.log("Bouton annuler cliqué");
                 annuler();
                 break;
+            case "Retour":
+                console.log("bouton retour cliqué");
+                returnHome();
             default:
                 console.log("Bouton inconnu cliqué" + buttonText);
                 break;
@@ -75,31 +66,21 @@ document.addEventListener("click", function (event) {
     }
 });
 
-function afficherClients() {
-    const divGeneral = document.getElementsByClassName("composantGrand")[0];
-    let saveDiv = divGeneral.innerHTML;
-    console.log("Afficher les clients");
-
-    divGeneral.innerHTML = ReactDOMServer.renderToString(<ListeClient />);
-    divGeneral.style.display = "block";
-}
-
-function voirAvantage() {
-    const divGeneral = document.getElementsByClassName("composantGrand")[0];
-    let saveDiv = divGeneral.innerHTML;
-    console.log("Afficher les clients");
-
-    divGeneral.innerHTML = ReactDOMServer.renderToString(<AvantageCarte />);
-    divGeneral.style.display = "block";
-}
-
 function annuler() {
-    const divGeneral = document.getElementsByClassName("composantGrand")[0];
-    let saveDiv = divGeneral.innerHTML;
-    console.log("annuler");
-
-    divGeneral.innerHTML = ReactDOMServer.renderToString(<AjouterCarte />);
+    const divGeneral = document.getElementsByClassName("dashboard-right")[0];
+    const smallIcons = ReactDOMServer.renderToString(<SmallIcons />);
+    const carteEnergie = ReactDOMServer.renderToString(<AjouterCarte />);
+    divGeneral.innerHTML = smallIcons + carteEnergie;
     divGeneral.style.display = "block";
 }
+
+function returnHome() {
+    const divGeneral = document.getElementsByClassName("dashboard-right")[0];
+    const smallIcons = ReactDOMServer.renderToString(<SmallIcons />);
+    const carteEnergie = ReactDOMServer.renderToString(<CarteEnergie />);
+    divGeneral.innerHTML = smallIcons + carteEnergie;
+    divGeneral.style.display = "block";
+}
+
 
 export default AjouterCarte;
