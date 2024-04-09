34c76bc8abcabb51eee853c1cfc156a38cd0e243
Essalah Sabra <sabra.essalah@etu.unice.fr>
2024-04-09 16:07:27 +0200

    transactions

--- frontend/src/components/transaction/TransactionGrand.js
+++ frontend/src/components/transaction/TransactionGrand.js
@@ -14,6 +14,9 @@ const transactionsDataAConfirmer = [
     { id_transaction: 1, date: '2024-03-01', quantité: 'Problème de pompe', prix: 20, statut: "En cours..." },
     { id_transaction: 2, date: '2024-03-02', quantité: 'Fuite de carburant', prix: 1, statut: "Terminé" },
     { id_transaction: 3, date: '2024-03-02', quantité: 'Fuite de carburant', prix: 1, statut: "Terminé" },
+    { id_transaction: 3, date: '2024-03-02', quantité: 'Fuite de carburant', prix: 1, statut: "Terminé" },
+    { id_transaction: 3, date: '2024-03-02', quantité: 'Fuite de carburant', prix: 1, statut: "Terminé" },
+
 ];
 
 const TransactionGrand = () => {
@@ -33,7 +36,7 @@ const TransactionGrand = () => {
         <div className='container-composantGrandTransaction'>
             <div className="Divflex">
                 <div id="topComposant">
-                    <h2 className="component_title2">💸 Transaction</h2>
+                    <h2 className="component_title2">💸 Transactions</h2>
                 </div>
             </div>
             <div className="separator"></div>
@@ -43,10 +46,10 @@ const TransactionGrand = () => {
                     <ul className="ul-transaction">
                         {transactions.map((transaction, index) => (
                             <li key={index}>
-                                <strong>Transaction n°</strong> {transaction.id_transaction},<br />
+                                <strong>Transaction n°</strong> {transaction.id_transaction}<br />
                                 <div className="separatorBlack"></div>
-                                <strong>Quantité:</strong> {transaction.quantité},<br />
-                                <strong>Total à payer:</strong> {transaction.prix} <span className="currency">euros</span>,<br />
+                                <strong>Quantité:</strong> {transaction.quantité}<br />
+                                <strong>Total à payer:</strong> {transaction.prix} <span className="currency">euros</span><br />
                                 <strong>Paiement:</strong> {transaction.statut}
                             </li>
                         ))}
@@ -54,42 +57,29 @@ const TransactionGrand = () => {
                 </div>
                 <div className="separatorColumn"></div>
                 <div className="right-container-transaction">
-                    <h3>Informations des transactions à confirmer</h3>
+                <h3>Informations des transactions à confirmer</h3>
+                <div className="ListeTransactionAConfirmer" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
                     <ul className="ul-transaction">
-                        {transactions.map((transaction, index) => (
-                            <div className="transaction-list-container">
-                                <div className="transaction-list-container" key={index}>
-                                    <div className="transaction-item">
-                                        <li>
-                                            <strong>Transaction n°</strong> {transaction.id_transaction},<br />
-                                            <div className="separatorBlack"></div>
-                                            <strong>Date de validation :</strong> {transaction.date},<br />
-                                            <strong>Montant total boutique :</strong> {transaction.prix} <span className="currency">euros</span>,<br />
-                                            <strong>Montant total énergie :</strong> {transaction.prix} <span className="currency">euros</span>,<br />
-                                            <strong>Montant total :</strong> {transaction.prix} <span className="currency">euros</span>,<br />
-                                            <p></p>
-                                            <button className="confirm-button" onClick={() => handleConfirm(transaction)}>Valider</button>
-                                            <button className="delete-button" onClick={() => handleDelete(transaction)}>Annuler</button>
-                                        </li>
-                                    </div>
-
-                                    <div className="transaction-item">
-                                        <li>
-                                            <strong>Transaction n°</strong> {transaction.id_transaction},<br />
-                                            <div className="separatorBlack"></div>
-                                            <strong>Date de validation :</strong> {transaction.date},<br />
-                                            <strong>Montant total boutique :</strong> {transaction.prix} <span className="currency">euros</span>,<br />
-                                            <strong>Montant total énergie :</strong> {transaction.prix} <span className="currency">euros</span>,<br />
-                                            <strong>Montant total :</strong> {transaction.prix} <span className="currency">euros</span>,<br />
-                                            <p></p>
-                                            <button className={`confirm-button-desactivate ${index === transactions.length - 1 ? "disabled" : ""}`} onClick={() => handleConfirm(transaction)}>Valider</button>
-                                        </li>
-                                    </div>
+                        {transactionsAConfirmer.map((transaction, index) => (
+                            <div className="transaction-list-container" key={index}>
+                                <div className="transaction-item">
+                                    <li>
+                                        <strong>Transaction n°</strong> {transaction.id_transaction}<br />
+                                        <div className="separatorBlack"></div>
+                                        <strong>Date de validation :</strong> {transaction.date}<br />
+                                        <strong>Montant total boutique :</strong> {transaction.prix} <span className="currency">euros</span><br />
+                                        <strong>Montant total énergie :</strong> {transaction.prix} <span className="currency">euros</span><br />
+                                        <strong>Montant total :</strong> {transaction.prix} <span className="currency">euros</span><br/>
+                                        <p></p>
+                                        <button className="confirm-button" onClick={() => handleConfirm(transaction)}>Valider</button>
+                                        <button className="delete-button" onClick={() => handleDelete(transaction)}>Annuler</button>
+                                    </li>
                                 </div>
                             </div>
                         ))}
                     </ul>
                 </div>
+            </div>
             </div>
         </div>
     );
